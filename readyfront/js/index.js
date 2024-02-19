072a9456e136813123aa538ff64670a33e7591ce
Yehor Hvozdov <evgwozdew@gmail.com>
2024-02-19 11:30:04 +0200

    Ready front part of project

---
+++ readyfront/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("open_modalBtn").addEventListener("click", function(){
+ document.getElementById("mymodal").classList.add("open")
+})
+
+document.getElementById("close").addEventListener("click", function(){
+ document.getElementById("mymodal").classList.remove("open")
+})
